2a279ff4d0978bea07d0461ab0e2bdc549a4f959
Miguel Angel de Dios <mddiosc@gmail.com>
2019-02-11 21:52:03 +0100

    Revert "renovado"
    This reverts commit 3e6f096dba4feb28987e719cf577884ed034a6f9.

--- creacionSitio/crearSitioWeb.sh
+++
@@ -1,9 +0,0 @@
-export DIRBASE=$PWD
-echo "Validando Usuario"
-sudo echo "Usuario Validado"
-echo $DIRBASE
-
-source ${DIRBASE}/configApache/creacionVhost.sh
-
-obtenerDatosDominio
-echo $nombreDominio
